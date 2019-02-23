044f0a3a664cfdbd7b7c5107d7e0243f17cdbb02
xyj <1451661318@qq.com>
2019-02-23 19:42:25 +0800

    1道题

--- LeetCode/src/com/study/simple/合并两个有序链表.java
+++ LeetCode/src/com/study/simple/合并两个有序链表.java
@@ -1,5 +1,6 @@
 package com.study.simple;
 
+import javax.swing.event.ListDataEvent;
 import java.util.List;
 
 /**
@@ -40,75 +41,40 @@ public class 合并两个有序链表 {
 
     /**
      * 将比对好的链表都插入到链表1中
+     *
      * @param l1
      * @param l2
      * @return
      */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-        int length1 = 0;
-        int length2 = 0;
-
-        //头指针的插入，，首数字要先进行判断,所有数字合成到那个链表中
-        if (l1.val > l2.val){
-            insertLists(l2,length2+1,l1.val);
-
-            l2 = getListNode(l2, l1, length2, length1);
-
-            return l2;
-        }else {
-
-            insertLists(l1,length1+1,l2.val);
-
-            l1 = getListNode(l1, l2, length1, length2);
-
-            return l1;
-        }
-
-
-    }
-
-    private ListNode getListNode(ListNode l1, ListNode l2, int length1, int length2) {
-        while ((l1 != null) || (l2 != null)){
-
-            //上面已经破案首链了，，所以下面先进行下个链
-            l1 = l1.next;
-            length1++;
-            l2 = l2.next;
-            length2++;
-
-            //现在是插入到l2 中，，所以要记录
-            if (l1.val > l2.val){
-                //将大的插入到后面
-                insertLists(l1,length1+1,l2.val);
-            }else {
-                //
-                insertLists(l1,length1+1,l2.val);
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
             }
         }
-        return l1;
+        // 任一为空，直接连接另一条链表
+        if (l1 == null) {
+            cur.next = l2;
+        } else {
+            cur.next = l1;
+        }
+        return dummyHead.next;
     }
+    public void printList(ListNode l1){
 
-    public ListNode insertLists(ListNode l1, int index , int value){
-
-        for (int i=0; i < index ;i++){
+        while (l1 != null){
+            System.out.print(l1.val + " ");
             l1 = l1.next;
         }
-        // 链表后面的链
-        ListNode temp ;
-        temp = l1.next.next;
-
-        //让链表后面都为空
-        l1.next.next = null;
-
-        //将值加到后面
-        l1.next.val =value;
-
-        //将后面的链，连接上
-        l1.next.next.next = temp;
-
-        return l1;
-
     }
     public static void main(String[] args) {
         合并两个有序链表 aa = new 合并两个有序链表();
@@ -116,17 +82,19 @@ public class 合并两个有序链表 {
         ListNode a = null;
 
         a = new ListNode(1);
-        a = new ListNode(2);
-        a = new ListNode(4);
+        a.next = new ListNode(2);
+        a.next.next = new ListNode(4);
 
         ListNode b = null;
 
-        a = new ListNode(1);
-        a = new ListNode(3);
-        a = new ListNode(4);
+        b = new ListNode(1);
+        b.next = new ListNode(3);
+        b.next.next = new ListNode(4);
 
-        aa.mergeTwoLists(a, b);
+        aa.printList(aa.mergeTwoLists(a,b));
+
     }
+
 }
 
 class ListNode {
