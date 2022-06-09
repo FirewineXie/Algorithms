32c4e73b1725a105f558b97a9ed81ba04cba2b41
Firewine <xyjworkgame@gmail.com>
2022-06-09 16:03:51 +0800

    完成t92

---
+++ LeetCode/src/com/category/linked/Question92.java
@@ -0,0 +1,120 @@
+package com.category.linked;
+
+
+import java.util.List;
+
+/**
+ * @author Firewine
+ * @version 1.0.0
+ * @ClassName Question92
+ * @createTime: 2022年06月08日 14:24:39
+ * @Description TODO
+ */
+public class Question92 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+
+        ListNode dummy = new ListNode();
+
+        dummy.next = head;
+
+        ListNode pre = dummy;
+
+        // 寻找前驱结点
+        for (int i = 1; i < left; i++) {
+            pre = pre.next;
+        }
+
+        head = pre.next;
+        for (int i = left; i < right; i++) {
+            ListNode nex = head.next;
+            head.next = nex.next;
+            nex.next = pre.next;
+            pre.next = nex;
+        }
+        return dummy.next;
+
+    }
+
+    public ListNode reverseBetween2(ListNode head, int left, int right) {
+        // 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        ListNode pre = dummyNode;
+        // 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
+        // 建议写在 for 循环里，语义清晰
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+
+        // 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
+        ListNode rightNode = pre;
+        for (int i = 0; i < right - left + 1; i++) {
+            rightNode = rightNode.next;
+        }
+
+        // 第 3 步：切断出一个子链表（截取链表）
+        ListNode leftNode = pre.next;
+        ListNode curr = rightNode.next;
+
+        // 注意：切断链接
+        pre.next = null;
+        rightNode.next = null;
+
+        // 第 4 步：同第 206 题，反转链表的子区间
+        reverseLinkedList(leftNode);
+
+        // 第 5 步：接回到原来的链表中
+        pre.next = rightNode;
+        leftNode.next = curr;
+        return dummyNode.next;
+    }
+
+    private void reverseLinkedList(ListNode head) {
+        // 也可以使用递归反转一个链表
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+    }
+
+    public ListNode reverseLinkedListDfs(ListNode head) {
+
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode tail = reverseLinkedListDfs(head.next);
+        head.next.next = head;
+        head.next = null;
+        return tail;
+    }
+
+}
+
+
+
---
+++ LeetCode/src/com/category/linked/Question92Test.java
@@ -0,0 +1,35 @@
+package com.category.linked;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Firewine
+ * @version 1.0.0
+ * @ClassName Question92Test
+ * @createTime: 2022年06月09日 14:22:34
+ * @Description TODO
+ */
+class Question92Test {
+
+    @Test
+    void reverseBetween() {
+
+
+        Question92 question92 = new Question92();
+        Question92.ListNode list1 = new Question92.ListNode(1);
+        Question92.ListNode list2 = new Question92.ListNode(2);
+        Question92.ListNode list3 = new Question92.ListNode(3);
+        Question92.ListNode list4 = new Question92.ListNode(4);
+        Question92.ListNode list5 = new Question92.ListNode(5);
+
+
+        list4.next = list5;
+        list3.next = list4;
+        list2.next = list3;
+        list1.next = list2;
+
+        question92.reverseLinkedListDfs(list1);
+    }
+}
